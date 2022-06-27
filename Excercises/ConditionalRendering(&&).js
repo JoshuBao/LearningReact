e420911b329c713e1ab47c95b65ae7683b204acf
Joshua Cheng <Joshuacheng888@gmail.com>
2022-06-27 14:44:48 -0700

    Update ConditionalRendering(&&).js

--- Excercises/ConditionalRendering(&&).js
+++ Excercises/ConditionalRendering(&&).js
@@ -36,3 +36,24 @@ export default function App() {
         </div>
     )
 }
+
+//Excercise 3
+import React from "react"
+
+export default function App() {
+    const [messages, setMessages] = React.useState(["a","b"])
+    /**
+     * Challenge:
+     * - If there are no unread messages, display "You're all caught up!"
+     * - If there are > 0 unread messages, display "You have <n> unread
+     *   message(s)"
+     *      - If there's exactly 1 unread message, it should read "message"
+     *        (singular)
+     */
+    return (
+        <div>
+            <p>{messages.length > 0? `You have ${messages.length} unread message${messages.length===1? "" :"s"}`:"You're all caught up!" }</p>
+        </div>
+    )
+}
+
