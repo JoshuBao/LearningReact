c90ea39a640c7c7419d1f0d80b11ecb4397697c8
Joshua Cheng <Joshuacheng888@gmail.com>
2022-06-29 19:57:27 -0700

    Update and rename ConditionalRenderingAndStateExcercise.js to WindowChallenges(CR,State).js

--- Excercises/WindowChallenges(CR,State).js
+++ Excercises/WindowChallenges(CR,State).js
@@ -34,3 +34,29 @@ export default function WindowTracker() {
         <h1>Window width: {window.innerWidth}</h1>
     )
 }
+
+
+//Challenge 2
+import React from "react"
+
+export default function WindowTracker() {
+    /**
+     * Challenge:
+     * 1. Create state called `windowWidth`, default to 
+     *    `window.innerWidth`
+     * 2. When the window width changes, update the state
+     * 3. Display the window width in the h1 so it updates
+     *    every time it changes
+     */
+    const [windowWidth,setWindowWidth] = React.useState(window.innerWidth)
+    React.useEffect(() => {
+        window.addEventListener("resize", function() {
+            console.log("Resized")
+            setWindowWidth(prevWidth=>window.innerWidth)
+        })
+    }, [])
+    
+    return (
+        <h1>Window width: {windowWidth}</h1>
+    )
+}
