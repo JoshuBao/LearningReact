52c51d48d7d14bef92e56c2c53d608935f97200c
Joshua Cheng <Joshuacheng888@gmail.com>
2022-07-01 18:43:50 -0700

    Create Notes-BumpRecentNote.js

---
+++ Excercises/Notes-BumpRecentNote.js
@@ -0,0 +1,16 @@
+function updateNote(text) {
+        // Try to rearrange the most recently-modified
+        // not to be at the top
+        setNotes(oldNotes => {
+            const newArray = []
+            for(let i = 0; i < oldNotes.length; i++) {
+                const oldNote = oldNotes[i]
+                if(oldNotes.id === currentNoteId) {
+                    newArray.unshift({ ...oldNote, body: text })
+                } else {
+                    newArray.push(oldNote)
+                }
+            }
+            return newArray
+        })
+        
