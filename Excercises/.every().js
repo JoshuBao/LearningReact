b31e38a11fbb0526cbdae32befabdd2dc5fb3131
Joshua Cheng <Joshuacheng888@gmail.com>
2022-07-05 18:03:51 -0700

    Create .every().js

---
+++ Excercises/.every().js
@@ -0,0 +1,9 @@
+ 
+    const allHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstValue)
+    if (allHeld && allSameValue) {
+      setTenzies(true)
+      console.log("You won!")
+    }
+//.every() looks for a specific condition, and if every item in an array meets that condition, .every() will return true as a whole
